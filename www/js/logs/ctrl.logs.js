6ac7099eb4fc8068ac56570f2f9b24e240af46aa
Alexis Mineaud <alexis.mineaud@gmail.com>
2015-03-23 17:02:00 +0100

    Move logs into a 'toggleable' panel.

--- www/js/logs/ctrl.logs.js
+++ www/js/logs/ctrl.logs.js
@@ -26,5 +26,61 @@
         });
       });
     }
-  ]);
+  ])
+
+  /**
+   * Show or hide the logs panel when hitting the 'square' key
+   * and listening on events to open/close the panel.
+   */
+  .directive('toggleConsole', ['$rootScope', function ($rootScope) {
+    return {
+      restrict: 'A',
+      link: function (scope, el) {
+        var $ = angular.element,
+            $panel = $(el);
+
+        $rootScope.$on('openLogs', function () {
+          $panel.addClass('opened');
+        });
+
+        $rootScope.$on('closeLogs', function () {
+          $panel.removeClass('opened');
+        });
+
+        $(document).keydown(function (e) {
+          if (e.which === 192) { // square key
+            $panel.toggleClass('opened');
+          }
+        });
+      }
+    };
+  }])
+
+  /**
+   * Broadcast an event to close the panel when clicking on the element.
+   */
+  .directive('closeLogs', ['$rootScope', function ($rootScope) {
+    return {
+      restrict: 'A',
+      link: function (scope, el) {
+        angular.element(el).click(function () {
+          $rootScope.$broadcast('closeLogs');
+        });
+      }
+    };
+  }])
+
+  /**
+   * Broadcast an event to open the panel when hovering the element.
+   */
+  .directive('openLogs', ['$rootScope', function ($rootScope) {
+    return {
+      restrict: 'A',
+      link: function (scope, el) {
+        angular.element(el).hover(function () {
+          $rootScope.$broadcast('openLogs');
+        });
+      }
+    };
+  }]);
 })();
